46fab60ee984cc7a26a47b2ac67efe9a1137375d
ERFAN <erfan.shokr300@gmail.com>
2024-01-14 13:17:47 +0330

    In this section we started complite frontend with create some new components

---
+++ app/vueapp/src/components/HomeVue.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Categories</h1>
+    <ul>
+      <li v-for="category in categories" :key="category.id">
+        {{ category.name }}
+      </li>
+    </ul>
+
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+
+export default {
+  data() {
+    return {
+      categories: [],
+      posts: [],
+    };
+  },
+  mounted() {
+    // در هنگام لود شدن Component، داده‌ها را از API درخواست کنید
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      api.getCategories().then((response) => {
+        this.categories = response.data;
+      });
+
+      api.getPosts().then((response) => {
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
