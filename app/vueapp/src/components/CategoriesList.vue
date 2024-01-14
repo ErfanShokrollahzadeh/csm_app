59521d70d31203d1ad0c744f263ff563a208fdee
ERFAN <erfan.shokr300@gmail.com>
2024-01-14 13:19:02 +0330

    Add CategoriesList and PostsList components

---
+++ app/vueapp/src/components/CategoriesList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Categories</h2>
+    <ul>
+      <li v-for="category in categories" :key="category.id">
+        {{ category.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["categories"],
+};
+</script>
---
+++ app/vueapp/src/components/PostsList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Posts</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+};
+</script>
