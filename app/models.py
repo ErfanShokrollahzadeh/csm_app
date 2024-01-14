51cc06af1b3b0d832bda742f2753ba10240a0415
ERFAN <erfan.shokr300@gmail.com>
2024-01-14 13:04:16 +0330

    Add Category and Post models
    Update models.py to include Category and Post models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
