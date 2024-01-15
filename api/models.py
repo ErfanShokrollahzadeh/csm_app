6adb9d48f446ac82491c14cf510316e4b3c096b6
ERFAN <erfan.shokr300@gmail.com>
2024-01-15 18:55:24 +0330

    Add 'api' and 'app' to INSTALLED_APPS and create modesl for api again

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    desceiption = models.BooleanField(default=False)
+    priority = models.IntegerField(default=1)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Todo"
+        verbose_name_plural = "Todos"
