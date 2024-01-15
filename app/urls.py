67e4a806ba224e0a557491421ab2df36e7d386c4
ERFAN <erfan.shokr300@gmail.com>
2024-01-15 15:25:26 +0330

    Delete unnecessary files and code

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from .views import category, post, home
-
-urlpatterns = [
-    path('categories/', category, name='category-list'),
-    path('posts/', post, name='post-list'),
-]
