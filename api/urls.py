c70c77d6aca298a7a34475029f85832f5a806ec4
ERFAN <erfan.shokr300@gmail.com>
2024-01-16 20:54:24 +0330

    Update API URLs and add JSON endpoint for todos

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.todo_json, name='index'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from rest_framework.request import Request  # new
+from rest_framework.response import Response  # new
+from rest_framework import status  # new
+from rest_framework.decorators import api_view  # new
 from . import models
 
 
@@ -7,3 +12,11 @@ def index(request):
         'todos': models.Todo.objects.order_by('priority').all()
     }
     return render(request, 'index.html', context)
+
+
+@api_view(['GET'])
+def todo_json(request: Request):
+    todos = list(models.Todo.objects.order_by(
+        'priority').all().values('title', 'is_done'))
+
+    return Response({'todos': todos}, status.HTTP_200_OK)
