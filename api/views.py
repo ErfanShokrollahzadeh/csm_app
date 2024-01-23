a0b53253873373f0cbfed93162a12ee5397486e3
ERFAN <erfan.shokr300@gmail.com>
2024-01-23 20:19:29 +0330

    create serializer to get us data for any data I wnat to show us

--- api/views.py
+++ api/views.py
@@ -8,8 +8,16 @@ from .models import Todo
 from .serializer import TodoSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def all_todos(request: Request):
-    todos = Todo.objects.order_by('priority').all()
-    todos_serializer = TodoSerializer(todos, many=True)
-    return Response(todos_serializer.data, status.HTTP_200_OK)
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(None, status=status.HTTP_400_BAD_REQUEST)
+    return Response(None, status=status.HTTP_400_BAD_REQUEST)
