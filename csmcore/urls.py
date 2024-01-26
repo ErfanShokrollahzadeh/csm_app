d6fd19171da18ca4095557ef693850c70ea40b78
ERFAN <erfan.shokr300@gmail.com>
2024-01-26 19:21:06 +0330

    Update API URL path

--- csmcore/urls.py
+++ csmcore/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', include(('api.urls'))),
+    path('api/', include(('api.urls'))),
     path('api-auth/', include('rest_framework.urls')),
 
 ]
