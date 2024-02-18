d78f501b99cf8b168fe8d75519c3984c3dfb79cb
ERFAN <erfan.shokr300@gmail.com>
2024-02-18 19:51:01 +0330

    Update ForgotPass component and main.js

--- app/vueapp/src/components/ForgotPass/ForgotPass.vue
+++ app/vueapp/src/components/ForgotPass/ForgotPass.vue
@@ -14,21 +14,45 @@
     </p>
   </div>
 
-  <div class="form-check form-check-reverse">
+  <div class="form-check form-check selectradio">
     <input class="form-check-input" type="radio" value="" id="reverseCheck1" />
     <label class="form-check-label" for="reverseCheck1">
-      Reverse checkbox
+      <p>via Email: <br />example@youremail.com</p>
     </label>
   </div>
-  <div class="form-check form-check-reverse">
+  <div class="form-check form-check selectradio">
     <input class="form-check-input" type="radio" value="" id="reverseCheck1" />
     <label class="form-check-label" for="reverseCheck1">
-      Reverse checkbox
+      <p>via SMS: <br />+62-8421-4512-2531</p>
     </label>
+  </div>
+
+  <p v-if="error" class="error-text">Please select an option</p>
+
+  <div class="button-container">
+    <button type="button" class="btn btn-primary btnforgot">Submit</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script>
+export default {
+  data() {
+    return {
+      selectedOption: "",
+      error: false,
+    };
+  },
+  methods: {
+    validateForm() {
+      if (!this.selectedOption) {
+        this.error = true;
+      } else {
+        // Continue with form submission
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 .back {
@@ -51,5 +75,20 @@ p {
   padding-top: 5px;
   color: #4e4b66;
   font-weight: 300;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 25rem;
+}
+.btnforgot {
+  width: 90%;
+}
+.selectradio {
+  margin-left: 10px;
+  background-color: #eef1f4;
+}
+.error-text {
+  color: red;
 }
 </style>
--- app/vueapp/src/main.js
+++ app/vueapp/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
-// import "bootstrap";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(fas);
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+
+// .component("fa", FontAwesomeIcon)
