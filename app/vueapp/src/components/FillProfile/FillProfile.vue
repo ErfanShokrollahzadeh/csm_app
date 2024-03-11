7679bf787851a2fd75eb09617aa581a0c9d0c9e7
ERFAN <erfan.shokr300@gmail.com>
2024-03-11 20:31:03 +0330

    create fiil profile page and finished and still needs some updating from front end section

--- app/vueapp/src/components/FillProfile/FillProfile.vue
+++ app/vueapp/src/components/FillProfile/FillProfile.vue
@@ -5,7 +5,7 @@
   </div>
 
   <div>
-    <form>
+    <form class="register">
       <div class="profile-photo">
         <label for="photo-upload" class="photo-label">
           <input
@@ -15,6 +15,24 @@
             @change="handlePhotoUpload"
           />
         </label>
+      </div>
+
+      <label class="input1"> Username</label>
+      <input type="text" class="input2" />
+
+      <label class="input1"> Full Name</label>
+      <input type="text" class="input2" />
+
+      <label class="input1">Email Address <span>*</span></label>
+      <input type="email" class="input2" />
+
+      <label class="input1">Phone Number <span>*</span></label>
+      <input type="number" class="input2" />
+
+      <div class="button-container">
+        <button type="button" class="btn btn-primary btnforgot" @click="goto">
+          Next
+        </button>
       </div>
     </form>
   </div>
@@ -63,5 +81,50 @@ export default {};
   border-radius: 50%;
   border: 1px solid #4e4b66;
   background-color: #eef1f4;
+}
+.register {
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+}
+.input1 {
+  margin-top: 10px;
+  font-weight: 700;
+  color: #4e4b66;
+}
+.input2 {
+  height: 45px;
+  border-radius: 5px;
+  border: 1px solid #4e4b66;
+}
+span {
+  color: red;
+  font-size: 20px;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+@media (max-width: 992px) {
+  .button-container {
+    margin-top: 16rem;
+  }
+}
+
+/* For mobile phones */
+@media (max-width: 380px) {
+  .button-container {
+    margin-top: 0rem;
+  }
+}
+@media (max-width: 320px) {
+  .button-container {
+    margin-top: 0rem;
+  }
+}
+.btnforgot {
+  width: 90%;
+  height: 45px;
+  font-weight: 700;
 }
 </style>
