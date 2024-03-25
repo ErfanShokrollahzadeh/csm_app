2dc0d53b96ea0848ba1befb369d12b48ef7beb2d
ERFAN <erfan.shokr300@gmail.com>
2024-03-25 13:06:03 +0330

    Add password for admin panel

--- csmcore/settings.py
+++ csmcore/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+# the pass of admoin panel is cms$#73&
+
 import os
 from pathlib import Path
 
